d22feb3e8cee41c831e2ac2b4f805e86be4c4855
GlaucoB0 <glauco.m.b.soares@gmail.com>
2024-07-10 08:26:29 -0300

    Conceitos sobre request

--- 1_express/server.js
+++ 1_express/server.js
@@ -4,8 +4,24 @@ const PORT = 3333
 
 const app = express()
 
+//Aceitar JSON
+app.use(express.json())
+
 //Rotas
+/** Request HTTP
+ * query params - ...:3333/pessoas?nome="Carlos"&idade=32
+ *  Rotas do tipo GET (filtros e buscas)
+ * route params - ...:3333/pessoas/5
+ *  Rotas do tipo GET, PUT, PATCH, DELETE(listar um elemento)
+ * body params - ...:3333/pessoas
+ *  Rotas do tipo POST (Cadastro de informações)
+ * 
+ */
+//Query Params
 app.get("/users", (request, response)=>{
+    //const query = request.query
+    const {nome, idade} = request.query
+    console.log(nome, idade)
     response.status(200).json([
         "Pessoa 1",
         "Pessoa 2",
@@ -14,6 +30,8 @@ app.get("/users", (request, response)=>{
 })
 
 app.post("/users", (request, response)=>{
+    const body = request.body
+    console.log(body)
     response.status(201).json([
         "Pessoa 1",
         "Pessoa 2",
@@ -22,7 +40,14 @@ app.post("/users", (request, response)=>{
     ])
 })
 
-app.put("/users", (request, response)=>{
+//Routes Params
+app.put("/users/:id/:cpf", (request, response)=>{
+    //const params = request.params
+    //console.log(params)
+    // const id = request.params.id
+    // const cpf = request.params.cpf
+    const {id, cpf} = request.params
+    console.log(id, cpf)
     response.status(200).json([
         "Pessoa 1",
         "Pessoa 10",
