ab727b82ee6bb2e7ba1084a5291698eda685fb95
GlaucoB0 <glauco.m.b.soares@gmail.com>
2024-07-22 08:25:16 -0300

    :lock: feat: Adicinada uma nova camada de segurança as consultas SQL

--- 2_2_livraria/src/controllers/livrosController.js
+++ 2_2_livraria/src/controllers/livrosController.js
@@ -44,12 +44,20 @@ export const postLivros = (request, response) => {
   // cadastrar um livro -> antes preciso saber se esse livro existe
   const checkSql = /*sql*/ `
     SELECT * FROM livros 
-    WHERE titulo = "${titulo}" AND 
-    autor = "${autor}" AND 
-    ano_publicacao = "${ano_publicacao}"
+    WHERE ?? = ? AND 
+    ?? = ? AND 
+    ?? = ?
     `;
+  const checkSqlData = [
+    "titulo",
+    titulo,
+    "autor",
+    autor,
+    "ano_publicacao",
+    ano_publicacao,
+  ];
 
-  conn.query(checkSql, (err, data) => {
+  conn.query(checkSql, checkSqlData, (err, data) => {
     if (err) {
       response.status(500).json({ message: "Erro ao buscar os livros" });
       return console.log(err);
@@ -66,10 +74,28 @@ export const postLivros = (request, response) => {
     const disponibilidade = 1; // aqui coloca 1 pq o banco de dados lá vai interpretar que está disponivel, e se estamos cadastrando é pq iremos ter o livro disponível
 
     // agora vamos cadastrar as informações
-    const insertSql = /*sql*/ `insert into livros(id, titulo, autor, ano_publicacao, genero, preco, disponibilidade) 
-        values("${id}","${titulo}","${autor}","${ano_publicacao}","${genero}","${preco}","${disponibilidade}");`;
+    const insertSql = /*sql*/ `
+      INSERT INTO livros(??, ??, ??, ??, ??, ??, ??) VALUES (?,?,?,?,?,?,?);
+      `;
 
-    conn.query(insertSql, (err) => {
+    const insertData = [
+      "livro_id",
+      "titulo",
+      "autor",
+      "ano_publicacao",
+      "genero",
+      "preco",
+      "disponibilidade",
+      id,
+      titulo,
+      autor,
+      ano_publicacao,
+      genero,
+      preco,
+      disponibilidade,
+    ];
+
+    conn.query(insertSql, insertData, (err) => {
       if (err) {
         response.status(500).json({ message: "Erro ao cadastrar o livro" });
         return console.log(err);
@@ -79,97 +105,120 @@ export const postLivros = (request, response) => {
   });
 };
 
-export const buscarLivro = (request, response)=>{
-  const {id} = request.params
+export const buscarLivro = (request, response) => {
+  const { id } = request.params;
 
-  const sql = /*sql*/`SELECT * FROM livros WHERE id = "${id}"`
-  conn.query(sql, (err, data)=>{
-      if(err){
-          console.error(err)
-          response.status(500).json({msg:"Erro ao  buscar livro"})
-          return
-      }
-      if(data.length === 0){
-          response.status(404).json({msg: "Livro não encontrado"})
-          return
-      }
-      response.status(200).json(data)
-  })
-}
+  const sql = /*sql*/ `SELECT * FROM livros WHERE ?? = ?`;
+  const insertData = ["livro_id", id];
 
-export const editarLivro = (request, response)=>{
-  const {id} = request.params
-  const {titulo, autor, ano_publicacao, genero, preco, disponibilidade} = request.body
+  conn.query(sql, insertData, (err, data) => {
+    if (err) {
+      console.error(err);
+      response.status(500).json({ msg: "Erro ao  buscar livro" });
+      return;
+    }
+    if (data.length === 0) {
+      response.status(404).json({ msg: "Livro não encontrado" });
+      return;
+    }
+    response.status(200).json(data);
+  });
+};
+
+export const editarLivro = (request, response) => {
+  const { id } = request.params;
+  const { titulo, autor, ano_publicacao, genero, preco, disponibilidade } =
+    request.body;
 
   //validações
-  if(!titulo){
-      response.status(400).json({message: 'O título é obrigatório!'})
-      return
+  if (!titulo) {
+    response.status(400).json({ message: "O título é obrigatório!" });
+    return;
   }
-  if(!autor){
-      response.status(400).json({message: 'O autor é obrigatório!'})
-      return
+  if (!autor) {
+    response.status(400).json({ message: "O autor é obrigatório!" });
+    return;
   }
-  if(!ano_publicacao){
-      response.status(400).json({message: 'O ano de publicação é obrigatório!'})
-      return
+  if (!ano_publicacao) {
+    response
+      .status(400)
+      .json({ message: "O ano de publicação é obrigatório!" });
+    return;
   }
-  if(!genero){
-      response.status(400).json({message: 'O genero é obrigatório!'})
-      return
+  if (!genero) {
+    response.status(400).json({ message: "O genero é obrigatório!" });
+    return;
   }
-  if(!preco){
-      response.status(400).json({message: 'O preco é obrigatório!'})
-      return
+  if (!preco) {
+    response.status(400).json({ message: "O preco é obrigatório!" });
+    return;
   }
-  if(disponibilidade === undefined){
-      response.status(400).json({msg: "Disponibilidade é obrigatória"})
+  if (disponibilidade === undefined) {
+    response.status(400).json({ msg: "Disponibilidade é obrigatória" });
   }
 
   //consultas
-  const checkSql = /*sql*/`SELECT * FROM livros WHERE id = "${id}"`
-  conn.query(checkSql, (err, data)=>{
-      if(err){
-          console.error(err)
-          response.status(500).json({msg:"Erro ao buscar livros"})
-          return
+  const checkSql = /*sql*/ `SELECT * FROM livros WHERE ?? = ?`;
+  const insertData = ["livro_id", id];
+  conn.query(checkSql, insertData, (err, data) => {
+    if (err) {
+      console.error(err);
+      response.status(500).json({ msg: "Erro ao buscar livros" });
+      return;
+    }
+    if (data.length === 0) {
+      return response.status(404).json({ msg: "livro não encontrado" });
+    }
+
+    //Consulta SQL para atualizar livro
+    const upadateSql = /*sql*/ `UPDATE livros SET
+      ?? = ?, ?? = ?, ?? = ?,
+      ?? = ?, ?? = ?, ?? = ?
+      WHERE ?? = ?`;
+
+    const checkSqlData = [
+      "titulo",
+      titulo,
+      "autor",
+      autor,
+      "ano_publicacao",
+      ano_publicacao,
+      "genero",
+      genero,
+      "preco",
+      preco,
+      "disponibilidade",
+      disponibilidade,
+      "livro_id",
+      id,
+    ];
+
+    conn.query(upadateSql, checkSqlData, (err) => {
+      if (err) {
+        console.error(err);
+        response.status(500).json({ msg: "Erro ao atualizar livro" });
+        return;
       }
-      if(data.length === 0){
-          return response.status(404).json({msg: "livro não encontrado"})
-      }
+      response.status(200).json({ msg: "livro atualizado" });
+    });
+  });
+};
 
-      //Consulta SQL para atualizar livro
-      const upadateSql = /*sql*/`UPDATE livros SET
-      titulo = "${titulo}", autor = "${autor}", ano_publicacao = "${ano_publicacao}",
-      genero = "${genero}", preco = "${preco}", disponibilidade = "${disponibilidade}"
-      WHERE id = "${id}"`
+export const deletarLivro = (request, response) => {
+  const { id } = request.params; // pega o id que for passado na rota
 
-      conn.query(upadateSql, (err)=>{
-          if(err){
-              console.error(err)
-              response.status(500).json({msg: "Erro ao atualizar livro"})
-              return
-          }
-          response.status(200).json({msg:"livro atualizado"})
-      })
-  })
-}
+  const deleteSql = /*sql*/ `delete from livros where id = "${id}"`;
+  conn.query(deleteSql, (err, info) => {
+    if (err) {
+      console.error(err);
+      response.status(500).json({ message: "Erro ao deletar livro" });
+    }
+    console.log(info);
+    if (info.affectedRows === 0) {
+      response.status(404).json({ messagae: "Livro não encontrado" });
+      return;
+    }
 
-export const deletarLivro = (request, response)=>{
-    const {id} = request.params // pega o id que for passado na rota
-
-    const deleteSql = /*sql*/ `delete from livros where id = "${id}"`
-    conn.query(deleteSql, (err, info)=>{
-        if(err){
-            console.error(err)
-            response.status(500).json({message: 'Erro ao deletar livro'})
-        }
-        console.log(info)
-        if(info.affectedRows === 0){
-            response.status(404).json({messagae: "Livro não encontrado"})
-            return
-        }
-
-        response.status(200).json({message: 'Livro selecionado foi deletado'})
-    })
-}
+    response.status(200).json({ message: "Livro selecionado foi deletado" });
+  });
+};
