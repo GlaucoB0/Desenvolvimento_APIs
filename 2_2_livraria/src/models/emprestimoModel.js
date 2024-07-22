4e3535309cdf19462e55845663b31e27726d0eb5
GlaucoB0 <glauco.m.b.soares@gmail.com>
2024-07-22 09:43:07 -0300

    :card_file_box: raw: coluna emprestimo_id alterada para int auto_increment

--- 2_2_livraria/src/models/emprestimoModel.js
+++ 2_2_livraria/src/models/emprestimoModel.js
@@ -2,13 +2,13 @@ import conn from "../config/conn.js";
 
 const tableEmprestimos = /*sql*/ `
     CREATE TABLE IF NOT EXISTS emprestimos(
-        emprestimo_id VARCHAR(60) PRIMARY KEY not null,
+        emprestimo_id int auto_increment PRIMARY KEY,
         cliente_id varchar(60) not null,
         foreign key (cliente_id) references clientes(cliente_id),
         livro_id varchar(60) not null,
         foreign key (livro_id) references livros(livro_id),
-        data_emprestimo datetime not null,
-        data_devolucao datetime not null,
+        data_emprestimo date not null,
+        data_devolucao date not null,
         created_at timestamp default current_timestamp,
         updated_at timestamp default current_timestamp on update current_timestamp
     );
@@ -20,5 +20,5 @@ conn.query(tableEmprestimos, (err, result, field) => {
     return;
   }
 
-  console.log("Tabela [Clientes] criada com sucesso!");
+  console.log("Tabela [emprestimos] criada com sucesso!");
 });
