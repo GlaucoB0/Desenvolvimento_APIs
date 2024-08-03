756202bf096811c496c753280d4a1eb3b55f03a7
GlaucoB0 <glauco.m.b.soares@gmail.com>
2024-08-03 09:43:40 -0300

    aaaaaaaaaaaaaabbbbbb

--- 3_DESAPEGA/src/controllers/objectControllers.js
+++ 3_DESAPEGA/src/controllers/objectControllers.js
@@ -1,7 +1,80 @@
+import getToken from "../helpers/get-token.js";
+import getUserByToken from "../helpers/get-user-by-token.js";
+import { v4 as uuidv4 } from "uuid";
+import conn from "../config/conn.js";
 
 export const create = async (request, response) => {
-    const {nome, peso, cor, descricao} = request.body
-    const disponivel = 1
+  const { nome, peso, cor, descricao } = request.body;
+  const disponivel = 1;
 
-    response.send("dddddddd")
-}
+  // Buscar o token do usuario cadastrado
+  const token = getToken(request);
+  const user = await getUserByToken(token);
+
+  if (!nome) {
+    return response.status(400).json({ msg: "O nome do objeto é obrigatório" });
+  }
+  if (!peso) {
+    return response.status(400).json({ msg: "O peso do objeto é obrigatório" });
+  }
+  if (!cor) {
+    return response.status(400).json({ msg: "O cor do objeto é obrigatório" });
+  }
+  if (!descricao) {
+    return response
+      .status(400)
+      .json({ msg: "O descricao do objeto é obrigatório" });
+  }
+
+  const objeto_id = uuidv4();
+  const usuario_id = user.usuario_id;
+  const insertSql = /*sql*/ `
+        INSERT INTO objects (??, ??, ??, ??, ??, ??, ??)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    `;
+  const insertData = [
+    "objeto_id",
+    "nome",
+    "peso",
+    "cor",
+    "descricao",
+    "usuario_id",
+    "disponivel",
+    objeto_id,
+    nome,
+    peso,
+    cor,
+    descricao,
+    usuario_id,
+    disponivel,
+  ];
+
+  conn.query(insertSql, insertData, (err, data)=>{
+    if(err){
+        console.error(err)
+        return response.status(500).json({err: "Erro ao cadastrar objeto"})
+    }
+
+    if(!request.files){
+        // Cadastrar no banco
+        const insertImagesSql = /*sql*/`
+            INSERT INTO object_images (image_id, objeto_id, image_path)
+            VALUES ?
+        `
+        const imageValues = request.files.map((file)=>[
+            uuidv4(),
+            objeto_id,
+            file.filename
+        ])
+        conn.query(insertImagesSql, [imageValues], (err)=>{
+            if(err){
+                console.error(err)
+                return response.status(500).json({err: "Erro ao cadastrar objeto"})
+            }
+            response.status(201).json({msg: "objeto criado"})
+        })
+    } else {
+
+    }
+  })
+};
--- 3_DESAPEGA/src/routes/objectRoutes.js
+++ 3_DESAPEGA/src/routes/objectRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { create } from "../controllers/objectControllers.js";
+import imageUpload from "../helpers/image-upload.js";
 import verifyToken from "../helpers/verify-token.js";
 
 
 const router = Router();
 
 //localhost:3333/objetos/
-router.post("/", verifyToken, create);
+router.post("/", verifyToken, imageUpload.array("imagens", 10), create);
 
 export default router;
